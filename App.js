ba807c4a6a6ec37487bfb383207fd998acfb3505
Nayan Joshi <50129883+Nayan-joshi07@users.noreply.github.com>
2022-10-22 19:13:32 +0530

    Added the Style for the Text

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import {Text , View , StyleSheet , SafeAreaView , Platform , StatusBar} from 'react-native';
+import { colors } from './src/utils/Color';
 
 export default function App() {
      return(
       <SafeAreaView style={styles.container}>
-        <Text>Hello World!</Text>
+        <Text style={styles.text}>Hello World!</Text>
       </SafeAreaView>
      );
 }
@@ -13,5 +14,9 @@ const styles = StyleSheet.create({
      container :{
       flex : 1,
       paddingTop : Platform.OS === 'android' ? StatusBar.currentHeight : 0, 
+      backgroundColor:  colors.blue
      },
+     text : {
+          color : colors.white
+     }
 });
